8ec29fd122a8143cc766c94b3c085ad5824883ab
Amir Aijaz <77732195+amirejaz@users.noreply.github.com>
2023-06-14 01:34:37 +0500

    Create Car.java

---
+++ Association/Composition/Car.java
@@ -0,0 +1,12 @@
+//Car must have Engine
+public class Car {
+   //engine is a mandatory part of the car
+   private final Engine engine;
+
+   public Car () {
+      engine = new Engine();
+   }
+}
+
+//Engine Object
+class Engine {}
