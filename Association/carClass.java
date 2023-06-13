40f7c30c18f7f79ec9115e422421fb825822aad7
Amir Aijaz <77732195+amirejaz@users.noreply.github.com>
2023-06-14 01:32:56 +0500

    Create carClass.java

---
+++ Association/carClass.java
@@ -0,0 +1,13 @@
+package association;
+
+public class carClass {
+    String carName;
+    int carID;
+
+    carClass(String cName, int cID){
+        this.carName = cName;
+        this.carID = cID;
+    }
+    
+}
+
