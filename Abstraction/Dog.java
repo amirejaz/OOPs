637de94ec797a37001e2c50ab0863606c769f00e
Amir Aijaz <77732195+amirejaz@users.noreply.github.com>
2023-06-14 01:28:33 +0500

    Add files via upload

---
+++ Abstraction/Dog.java
@@ -0,0 +1,14 @@
+package Abstraction;
+
+//Dog class extends Animal class
+public class Dog extends Animal{
+
+   public void sound(){
+	System.out.println("Woof");
+   }
+   public static void main(String args[]){
+	Animal obj = new Dog();
+	obj.sound();
+   }
+}
+
